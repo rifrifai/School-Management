21a06349fefbef6a98595a5c5055d9ca8e5e4686
AnwarHakim <anwarhakim31@gmail.com>
2024-09-09 16:26:14 +0700

    finish

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -94,7 +94,7 @@ const LoginPage = () => {
           <Slider />
         </div>
         <div className="bg-background py-24 sm:py-0 w-full h-screen sm:order-2 flex justify-center flex-col px-14 sm:px-8 md:px-14 lg:px-28 xl:px-32 bg items-center">
-          <figure className="w-[200px] h-[45px]">
+          <figure className="w-[200px] h-[35px]">
             <img src={logo} alt="logo" className="mx-auto w-full h-full" />
           </figure>
           {/* <div className="h-2 w-full mt-14  mb-4 relative">
