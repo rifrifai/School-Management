36d1942323cb8f2fd245ed90021a3f88b8120f25
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-10-14 22:19:34 +0700

    initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,14 @@ import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://manajemen-sekolah-be.vercel.app/api",
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       //eslint-disable-next-line no-undef
