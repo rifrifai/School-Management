3ed7959d638e5365c430baddfced4c2e0f0def09
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-10-15 08:04:26 +0700

    edit target

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://manajemen-sekolah-be.vercel.app/api",
+        target: "https://manajemen-sekolah-be.vercel.app",
         changeOrigin: true,
       },
     },
